568d160fbaa8bf1b8d24aea6878de2cbbd6710e2
L_Smith_E <smith.e.laurence@gmail.com>
2023-01-13 13:45:10 -0500

    VS 2022 Commit: Updated Add and Delete Service Calls

--- ToDoMAUIClient/ToDoMAUIClient/DataServices/RestDataService.cs
+++ ToDoMAUIClient/ToDoMAUIClient/DataServices/RestDataService.cs
@@ -36,15 +36,68 @@ namespace ToDoMAUIClient.DataServices
 
 
 
-        public Task AddToDoAsync(ToDo toDo)
+        public async Task AddToDoAsync(ToDo toDo)
         {
-            throw new NotImplementedException();
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("---> No internet access...");
+                return;
+            }
+
+            try
+            {
+                string jsonToDo = JsonSerializer.Serialize<ToDo>(toDo, _jsonSerializerOptions);
+                StringContent content = new StringContent(jsonToDo, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _httpClient.PostAsync($"{_url}/todo", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("Successfully created ToDo");
+                }
+                else
+                {
+                    Debug.WriteLine("---> Non Http 2xx response");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Whoops exception: {ex.Message}");
+            }
+
+            return;
+
         }
 
-        public Task DeleteToDoAsync(int id)
+        public async Task DeleteToDoAsync(int id)
         {
-            throw new NotImplementedException();
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("---> No internet access...");
+                return;
+            }
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"{_url}/todo/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("Successfully created ToDo");
+                }
+                else
+                {
+                    Debug.WriteLine("---> Non Http 2xx response");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Whoops exception: {ex.Message}");
+            }
+
+            return;
         }
+
 
         public async Task<List<ToDo>> GetAllToDosAsync()
         {
